58530608e2b0e40eec8ac87e99326fdb2098830a
Ziraddin Ismayilov <103113391+Ziraddin@users.noreply.github.com>
2023-06-03 21:49:38 +0400

    Update MainActivity.kt

--- app/src/main/java/com/zireddinismayilov/vehicleapi/MainActivity.kt
+++ app/src/main/java/com/zireddinismayilov/vehicleapi/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         binding.getManufacturersRecyclerView.layoutManager = GridLayoutManager(this, 1)
 
